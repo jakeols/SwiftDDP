300b233b72c257c4085f5c3f7a1011c506173d0c
Jake Ols <jakeo@thinkglobalschool.com>
2019-11-09 15:25:41 -0800

    update targets, schemes

---
+++ SwiftDDP/SwiftDDP.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDDP.h
+//  SwiftDDP
+//
+//  Created by Jake Ols on 11/9/19.
+//  Copyright © 2019 Peter Siegesmund. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDDP.
+FOUNDATION_EXPORT double SwiftDDPVersionNumber;
+
+//! Project version string for SwiftDDP.
+FOUNDATION_EXPORT const unsigned char SwiftDDPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDDP/PublicHeader.h>
+
+
---
+++ SwiftDDP/SwiftDDP.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftDDP.swift
+//  SwiftDDP
+//
+//  Created by Jake Ols on 11/9/19.
+//  Copyright © 2019 Peter Siegesmund. All rights reserved.
+//
+
+class SwiftDDP {
+
+}
